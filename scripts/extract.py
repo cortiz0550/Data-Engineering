4dbd65c516ee102bd3dce5abfa1c63a63ebccc15
Chris Ortiz <cortiz0550@gmail.com>
2024-09-05 13:07:29 -0700

    fix error

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,10 +1,11 @@
 import requests
 import json
+import os
 
 def load_config(path):
     with open(path, 'r') as json_file:
         config = json.load(json_file)
-    
     print(config)
-
-load_config("C:\\Users\\E33100\\OneDrive - SRI International\\My Stuff\\Me\\PD\\Data-Engineering\\config\\qualtrics_config.json")    
+    
+path = os.getcwd()
+load_config(path + "\\config\\qualtrics_config.json")
